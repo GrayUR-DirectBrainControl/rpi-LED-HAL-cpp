2951adb07a0f2646944e17b0ba5e462f66b0d86f
directbraincontrol-VT <directbraincontrol.vt@gmail.com>
2025-02-17 17:00:05 -0500

    Added code to test brainflow library

--- ledcontrol.cpp
+++ ledcontrol.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include "pigpio.h" 
-
 #include <stdlib.h>
 
+#include "brainflow/src/board_controller.h"
 
-int main()
-{
-	std::cout << "Hello World!" << std::endl;
-	return 0; 
+int main() {
+    int board_id = (int)BoardIds::SYNTHETIC_BOARD;	//Testing brainflow library with synthetic board
+    BrainFlowInputParams params; 
+    BoardShim::set_log_file("brainflow.log");
+    BoardShim::enable_dev_board_logger(); 
+
+    try {
+        BoardShim board(board_id, params);
+        board.prepare_session();
+		//if(is_prepared()){ replaces with try-catch method
+			std::cout << "BrainFlow session started!" << std::endl;
+		//}
+        board.release_session();
+    } catch (const BrainFlowException& e) {
+        std::cerr << "BrainFlow error: " << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
 }
 
-//Test
+
